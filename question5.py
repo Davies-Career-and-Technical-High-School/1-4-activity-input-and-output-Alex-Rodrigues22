3b2e55bddfb520beadb08f6d57201110dc48fa9e
Alexander Rodrigues <alexander.rodrigues@daviestech.org>
2020-09-30 13:04:13 +0000

    complete

---
+++ question5.py
@@ -0,0 +1,3 @@
+word1 = input("enter a word")
+word2 = input("enter a word")
+print(word1 +  word2)
---
+++ question6.py
@@ -0,0 +1,3 @@
+first_name = input("Please input your first name:")
+last_name  = input("Please input your last name:")
+print(last_name +  first_name)
